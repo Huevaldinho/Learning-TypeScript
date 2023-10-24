4812c657bbdbed9c6d6959bd02822e0c7d92f483
Huevaldinho <felipeobando@estudiantec.cr>
2023-10-24 16:26:19 -0600

    Primer comit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { Animal } from "./types/animal.type";
+console.log("Hola mundo");
+
+console.log("Animal type: ", { name: "Ike", age: 10, legs: 4 } as Animal);
+
+let variableName: number = 4;
+function add(number1: number, number2: number): number {
+  return number1 + number2;
+}
